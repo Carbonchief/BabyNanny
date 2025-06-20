bbf85a2cb7dece5f537a6606d9732086086d92c9
Luan van der Walt <30735486+Carbonchief@users.noreply.github.com>
2025-06-20 11:45:48 +0200

    Add child management UI and repository updates

--- BabyNanny/Data/BabyNannyRepository.cs
+++ BabyNanny/Data/BabyNannyRepository.cs
@@ -7,55 +7,59 @@ namespace BabyNanny.Data
 {
     public class BabyNannyRepository(string dbPath)
     {
-        private SQLiteConnection? _connection;
+        private readonly string _dbPath = dbPath;
 
         public void Init()
         {
-            _connection = new SQLiteConnection(dbPath);
-            _connection.CreateTable<Child>();
-            _connection.CreateTable<BabyAction>();
+            using var _ = GetConnection();
+        }
 
-            var tableInfo = _connection.GetTableInfo(nameof(BabyAction));
+        private SQLiteConnection GetConnection()
+        {
+            var connection = new SQLiteConnection(_dbPath);
+            connection.CreateTable<Child>();
+            connection.CreateTable<BabyAction>();
+
+            var tableInfo = connection.GetTableInfo(nameof(BabyAction));
             var columns = tableInfo.Select(c => c.Name).ToList();
             if (!columns.Contains(nameof(BabyAction.FeedingType)))
-                _connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.FeedingType)} INTEGER");
+                connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.FeedingType)} INTEGER");
             if (!columns.Contains(nameof(BabyAction.AmountML)))
-                _connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.AmountML)} INTEGER");
+                connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.AmountML)} INTEGER");
             if (!columns.Contains(nameof(BabyAction.BottleType)))
-                _connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.BottleType)} TEXT");
+                connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.BottleType)} TEXT");
             if (!columns.Contains(nameof(BabyAction.MealDescription)))
-                _connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.MealDescription)} TEXT");
+                connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.MealDescription)} TEXT");
             if (!columns.Contains(nameof(BabyAction.DiaperType)))
-                _connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.DiaperType)} TEXT");
+                connection.Execute($"ALTER TABLE {nameof(BabyAction)} ADD COLUMN {nameof(BabyAction.DiaperType)} TEXT");
+            return connection;
         }
 
         #region Child
 
         public List<Child>? GetChildren()
         {
-            Init();
-            return _connection.GetAllWithChildren<Child>();
-            //return _connection?.Table<Child>().ToList();
-
+            using var db = GetConnection();
+            return db.GetAllWithChildren<Child>();
         }
 
         public Child AddChild(Child child)
         {
-            _connection = new SQLiteConnection(dbPath);
-            _connection.Insert(child);
+            using var db = GetConnection();
+            db.Insert(child);
             return child;
         }
 
         public void UpdateChild(Child child)
         {
-            _connection = new SQLiteConnection(dbPath);
-            _connection.Update(child);
+            using var db = GetConnection();
+            db.Update(child);
         }
 
         public void DeleteChild(int id)
         {
-            _connection = new SQLiteConnection(dbPath);
-            _connection.Delete<Child>(id);
+            using var db = GetConnection();
+            db.Delete<Child>(id);
         }
 
         #endregion
@@ -64,8 +68,8 @@ namespace BabyNanny.Data
 
         public List<BabyAction>? GetActions()
         {
-            Init();
-            return _connection?.Table<BabyAction>().OrderByDescending(x => x.Started).ToList();
+            using var db = GetConnection();
+            return db.Table<BabyAction>().OrderByDescending(x => x.Started).ToList();
         }
 
         public bool AddAction(BabyAction? action)
@@ -73,30 +77,30 @@ namespace BabyNanny.Data
             if (action == null)
                 return false;
 
-            _connection = new SQLiteConnection(dbPath);
+            using var db = GetConnection();
 
-            var existing = _connection.Table<BabyAction>()
+            var existing = db.Table<BabyAction>()
                 .FirstOrDefault(a => a.ChildId == action.ChildId && a.Started != null && a.Stopped == null);
 
             if (existing != null)
                 return false;
 
-            _connection.Insert(action);
+            db.Insert(action);
             return true;
 
         }
 
         public void EditAction(BabyAction action)
         {
-            _connection = new SQLiteConnection(dbPath);
-            _connection.Update(action);
+            using var db = GetConnection();
+            db.Update(action);
 
         }
 
         public void DeleteAction(int id)
         {
-            _connection = new SQLiteConnection(dbPath);
-            _connection.Delete<BabyAction>(id);
+            using var db = GetConnection();
+            db.Delete<BabyAction>(id);
         }
 
         #endregion
